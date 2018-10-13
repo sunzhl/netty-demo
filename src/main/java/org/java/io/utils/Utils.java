c625621bd261d7904ae98f5c134ba5eb4d483afe
sun_zhl <sun_zhl7981@163.com>
2018-10-13 15:07:00 +0800

    添加日期格式化方法

--- src/main/java/org/java/io/utils/Utils.java
+++ src/main/java/org/java/io/utils/Utils.java
@@ -3,15 +3,15 @@ package org.java.io.utils;
 import java.util.Date;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 
 public class Utils {
 
 	public static final String LIMIT = "$_";
-	
+
 	public static byte[] getLimit() {
 		return "$_".getBytes();
 	}
-	
 
 	public static String getContent(final String body) {
 		String str = StringUtils.deleteWhitespace(body);
@@ -20,4 +20,11 @@ public class Utils {
 				: "BAD_ORDER";
 		return currentTime;
 	}
+
+	public static String dateFormat(String pattern) {
+
+		return DateFormatUtils.format(new Date(), pattern);
+
+	}
+
 }
